683c613234e3106d436250fbe105d6fb264dfaf8
Thales Alves <thalesalves758@gmail.com>
2022-09-26 14:49:14 -0300

    create a command to reset database

--- front-end/cypress/support/commands.js
+++ front-end/cypress/support/commands.js
@@ -23,3 +23,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("resetDatabase", () => {
+  cy.request("POST", `http://localhost:5000/database/reset`);
+});
