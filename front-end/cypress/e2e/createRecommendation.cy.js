721e8041148eb21ca16cb51cc9463244bcd96237
Thales Alves <thalesalves758@gmail.com>
2022-09-26 14:41:17 -0300

    test: add createRecommendation test

---
+++ front-end/cypress/e2e/createRecommendation.cy.js
@@ -0,0 +1,23 @@
+import { faker } from "@faker-js/faker";
+
+describe("create recommendation", () => {
+  it("should create a new recommendation", () => {
+    const recommendationFake = {
+      name: faker.music.songName(),
+      youtubeLink: "https://youtu.be/UNdQsRCFJjA",
+    };
+
+    cy.intercept("POST", "/recommendations").as("postRecommendation");
+
+    cy.visit("http://localhost:3000/");
+
+    cy.get('[data-cy="name"]').type(recommendationFake.name);
+    cy.get('[data-cy="youtubeLink"]').type(recommendationFake.youtubeLink);
+
+    cy.get('[data-cy="create-recommendation"]').click();
+
+    cy.wait("@postRecommendation");
+
+    cy.contains(recommendationFake.name).should("be.visible");
+  });
+});
